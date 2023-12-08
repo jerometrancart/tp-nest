931ec3d04561b8aeaa8db064530939d38f741c76
jerometrancart <jerome.trancart@gmail.com>
2023-12-08 15:41:28 +0100

    added register logic in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -27,4 +27,33 @@ export class AuthController {
 
     return { message: 'Disconnected successfully', status: HttpStatus.OK };
   }
+
+  @Post('register')
+  async register(@Request() req) {
+    const { username, password } = req.body;
+    const user = await this.authService.validateUser(username, password);
+
+    if (!user) {
+      // Si la validation échoue, vous pouvez retourner un message d'erreur approprié
+      return {
+        message: 'Invalid credentials for registration',
+        status: HttpStatus.UNAUTHORIZED,
+      };
+    }
+
+    // Logique pour créer l'utilisateur dans la base de données, par exemple avec Prisma
+    // Assurez-vous d'ajuster cela selon votre modèle et vos besoins spécifiques
+    const createdUser = await prisma.user.create({
+      data: {
+        username: user.username,
+        password: user.password, // Assurez-vous d'ajuster cela selon votre modèle
+      },
+    });
+
+    return {
+      message: 'Registration successful',
+      user: createdUser,
+      status: HttpStatus.CREATED,
+    };
+  }
 }
